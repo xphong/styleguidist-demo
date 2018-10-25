1acae57c042a4789ef0e82827b71004ac85baa89
xphong <xphong@gmail.com>
2018-10-24 20:55:23 -0400

    chore(lint): add lint

--- babel.config.js
+++ babel.config.js
@@ -5,36 +5,36 @@ module.exports = function(api) {
   }
   const presets = [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         modules: false,
-        targets: { browsers: ["last 2 versions", "ie >= 11"] },
-        include: ["es6.object.assign", "es6.promise", "es6.object.keys"],
-        useBuiltIns: "usage"
+        targets: { browsers: ['last 2 versions', 'ie >= 11'] },
+        include: ['es6.object.assign', 'es6.promise', 'es6.object.keys'],
+        useBuiltIns: 'usage'
       }
     ],
-    "@babel/preset-react"
+    '@babel/preset-react'
   ];
   const plugins = [
-    "@babel/plugin-proposal-export-default-from",
-    "@babel/plugin-proposal-logical-assignment-operators",
-    ["@babel/plugin-proposal-optional-chaining", { loose: false }],
-    ["@babel/plugin-proposal-pipeline-operator", { proposal: "minimal" }],
-    ["@babel/plugin-proposal-nullish-coalescing-operator", { loose: false }],
-    "@babel/plugin-proposal-do-expressions",
-    "babel-plugin-styled-components",
-    "@babel/transform-runtime",
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-transform-react-jsx-source",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-transform-modules-commonjs"
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-logical-assignment-operators',
+    ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
+    '@babel/plugin-proposal-do-expressions',
+    'babel-plugin-styled-components',
+    '@babel/transform-runtime',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-react-jsx-source',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-modules-commonjs'
   ];
 
   return {
     presets,
     plugins,
     ignore: [
-      "src/*.test.js"
+      'src/*.test.js'
     ]
   };
 };
